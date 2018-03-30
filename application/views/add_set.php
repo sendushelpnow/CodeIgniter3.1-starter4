04b68d2c05c630675babc6eed1b13f54815b2185
clai <clai-@hotmail.com>
2018-03-30 11:48:34 -0700

    slight update
    Signed-off-by: clai <clai-@hotmail.com>

--- application/views/add_set.php
+++ application/views/add_set.php
@@ -2,12 +2,23 @@
     Set Name:<input type="text" name="setname">
     Headwear:
     <select>
+        <option>Mage's Hat</option>
+        <option>Light Helm</option>
     </select>
     Armor:
-    <select></select>
+    <select>
+        <option>Mage's Robe</option>
+        <option>Light Armor</option>
+    </select>
     Weapon:
-    <select></select>
+    <select>       
+        <option>Wand</option>
+        <option>Sword</option>
+    </select>
     Footwear:
-    <select></select>
+    <select>
+        <option>Mage's Shoes</option>
+        <option>Light Boots</option>
+    </select>
     <div class="btn"><a href="maintenance/submit">Submit</a></div>
 </div>
